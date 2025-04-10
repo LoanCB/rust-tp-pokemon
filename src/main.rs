14b4deed419c8ae940ec9b15d52a58a5b727f225
Loan <loanbillonnet@gmail.com>
2025-04-10 18:05:05 +0200

    feat: sort list by level

--- src/main.rs
+++ src/main.rs
@@ -168,44 +168,60 @@ impl Breeding {
         sorted.sort_by(|a, b| a.pokemon_type.cmp(&b.pokemon_type));
         sorted
     }
+
+    fn list_by_level(&self) -> Vec<Pokemon> {
+        let mut sorted = self.pokemons.clone();
+        sorted.sort_by(|a, b| a.level.cmp(&b.level));
+        sorted
+    }
 }
 
 fn main() {
-    let mut breeding = Breeding::new();
-
-    let pikachu = Pokemon::new("Pikachu", PokemonType::Electrik, Gender::Male);
-    let salameche = Pokemon::new("Salamèche", PokemonType::Feu, Gender::Femelle);
-    let bulbizarre = Pokemon::new("Bulbizarre", PokemonType::Plante, Gender::Male);
-    let feunard = Pokemon::new("Feunard", PokemonType::Feu, Gender::Male);
-    let galifeu = Pokemon::new("Galifeu", PokemonType::Feu, Gender::Femelle);
-
-    breeding.add_pokemon(pikachu);
-    breeding.add_pokemon(salameche);
-    breeding.add_pokemon(bulbizarre);
-    breeding.add_pokemon(feunard);
-    breeding.add_pokemon(galifeu);
-
-    breeding.show_all();
-    breeding.train_pokemons(1125);
-    breeding.show_all();
-
-    let feunard_ref = breeding.pokemons[3].clone();
-    let galifeu_ref = breeding.pokemons[4].clone();
-    breeding.try_reproduce(&feunard_ref, &galifeu_ref);
-    breeding.show_all();
-
-    // save to file
-    let _ = breeding.save_to_file();
+    // let mut breeding = Breeding::new();
+    //
+    // let pikachu = Pokemon::new("Pikachu", PokemonType::Electrik, Gender::Male);
+    // let salameche = Pokemon::new("Salamèche", PokemonType::Feu, Gender::Femelle);
+    // let bulbizarre = Pokemon::new("Bulbizarre", PokemonType::Plante, Gender::Male);
+    // let feunard = Pokemon::new("Feunard", PokemonType::Feu, Gender::Male);
+    // let galifeu = Pokemon::new("Galifeu", PokemonType::Feu, Gender::Femelle);
+    //
+    // breeding.add_pokemon(pikachu);
+    // breeding.add_pokemon(salameche);
+    // breeding.add_pokemon(bulbizarre);
+    // breeding.add_pokemon(feunard);
+    // breeding.add_pokemon(galifeu);
+    //
+    // breeding.show_all();
+    // breeding.train_pokemons(1125);
+    // breeding.show_all();
+    //
+    // let feunard_ref = breeding.pokemons[3].clone();
+    // let galifeu_ref = breeding.pokemons[4].clone();
+    // breeding.try_reproduce(&feunard_ref, &galifeu_ref);
+    // breeding.show_all();
+    //
+    // // save to file
+    // let _ = breeding.save_to_file();
 
     // Load from file
-    // match Breeding::load_from_file() {
-    //     Ok(reloaded) => {
-    //         println!("Elevage rechargé !");
-    //         reloaded.show_all();
-    //     }
-    //     Err(e) => eprintln!("Erreur au chargement : {}", e),
-    // }
+    let mut breeding = Breeding::load_from_file()
+        .map(|b| {
+            println!("Elevage rechargé !");
+            b
+        })
+        .unwrap_or_else(|e| {
+            eprintln!("Erreur au chargement : {}", e);
+            Breeding::new()
+        });
 
+    // List basic
+    breeding.show_all();
+
+    // List by type
     println!("Liste des Pokémons triés par type");
     show_pokemons_list(&breeding.list_by_type());
+
+    // List by level
+    println!("Liste des Pokémons triés par niveau");
+    show_pokemons_list(&breeding.list_by_level());
 }
